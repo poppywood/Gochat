7259a912cd611630c5ca5a46219c7cbbb95af19a
Shelly Saunders <shelly@poppywood.eu>
2019-07-02 20:55:47 +0300

    init commit
    Allows two browsers to send messages to each other on locahost:8088

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	// get the room going as a new goroutine so chat happens in the background
+	go r.run()
+	// start the web server
+	if err := http.ListenAndServe(":8088", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
